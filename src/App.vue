b4d017e9cbd6414ab31fe1897566deace4f2dd72
zhangding <zhangding@shandagames.com>
2017-08-01 17:32:16 +0800

    阿萨德

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="app">
+  	<tab></tab>
     <router-view></router-view>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'app'
-}
+<script type="text/ecmascript-6">
+	import Tab from './components/tab/tab'
+	export default{
+		Tab
+	}
 </script>
 
 <style>
--- src/components/tab/tab.vue
+++ src/components/tab/tab.vue
@@ -1,16 +1,16 @@
 <template>
-	<ul class="nav">
+	<ul class="nav-list">
 		
 		<router-link tag="li" class="nav-item" to="/recommend">
 			推荐
 		</router-link>
-		<router-link tag="li" class="nav-item" to="/recommend">
+		<router-link tag="li" class="nav-item" to="/singer">
 			歌手
 		</router-link>
-		<router-link tag="li" class="nav-item" to="/recommend">
+		<router-link tag="li" class="nav-item" to="/rank">
 			排行
 		</router-link>
-		<router-link tag="li" class="nav-item" to="/recommend">
+		<router-link tag="li" class="nav-item" to="/search">
 			搜索
 		</router-link>
 	</ul>
@@ -19,7 +19,19 @@
 <script type="text/ecmascript-6">
 
 </script>
-<style scoped lang="sass" rel="stylesheet/sass">
-	@import "../../common/css/reset";
+<style scoped lang="scss" rel="stylesheet/scss">
+@import "../../common/css/reset";
+
+.nav-list{
+
+	display:flex;
+	.nav-item{
+		flex:1;
+
+	};
+	.router-link-active{
+		border:1px solid red
+	}
+}
 
 </style>
